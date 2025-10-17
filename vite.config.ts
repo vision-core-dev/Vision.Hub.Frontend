a37c91bdda679a6b5c890c0d5973933b7d3eef09
Kyrylo Bitsay <service@kyrylo.tech>
2025-10-17 13:35:13 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    server: {
+        host: "0.0.0.0",
+        port: 5173,
+        allowedHosts: ["hub.vcore.dev"]
+    }
 })
