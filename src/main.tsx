82e878082b7cbcc481fa091fda627fa9dc47d65d
Kyrylo Bitsay <service@kyrylo.tech>
2025-10-18 23:23:44 +0300

    Add ESLint import plugin and config for import rules
    Introduced .eslintrc.json with import plugin and no-unresolved rule to enforce case-sensitive import paths. Updated package.json and package-lock.json to include eslint-plugin-import as a dev dependency.

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import ProtectedRoute from "./components/System/ProtectedRoute";
 import Dashboard from "./components/Pages/Dashboard/DashboardPage.tsx";
 import Login from "./components/Pages/auth/Login/LoginPage.tsx";
 
-import UsersListPage from "./components/Pages/users/UsersListPage.tsx";
-import CreateUserPage from "./components/Pages/users/CreateUserPage.tsx";
-import UserDetailsPage from "./components/Pages/users/UserDetails/UserDetailsPage.tsx";
+import UsersListPage from "./components/Pages/Users/UsersListPage.tsx";
+import CreateUserPage from "./components/Pages/Users/CreateUserPage.tsx";
+import UserDetailsPage from "./components/Pages/Users/UserDetails/UserDetailsPage.tsx";
 import CreateEventPage from "./components/Pages/Events/CreateEventPage.tsx";
 import EventsListPage from "./components/Pages/Events/EventsListPage.tsx";
 import AccountDeactivated from "./components/Pages/auth/AccountDeactivated/AccountDeactivated.tsx";
