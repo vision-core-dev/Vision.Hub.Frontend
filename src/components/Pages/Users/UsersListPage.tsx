75321f32b010dfdbd22cdc40fc0870c153d049ca
Kyrylo Bitsay <service@kyrylo.tech>
2025-11-06 22:14:12 +0200

    Split users list into active and archived sections
    Refactored UsersListPage to separately display active and archived users, improving clarity and user management. Added user count state and updated table rendering logic to handle both categories.

--- src/components/Pages/Users/UsersListPage.tsx
+++ src/components/Pages/Users/UsersListPage.tsx
@@ -6,6 +6,7 @@ import UserLabel from "../../basic/User/UserLabel.tsx";
 import DefaultPage from "../../basic/DefaultPage/DefaultPage.tsx";
 import Button from "../../basic/Button/Button.tsx";
 import {Plus} from "lucide-react";
+import type {UserRoleType} from "../../../types/Users.ts";
 
 interface User {
     id: string;
@@ -14,56 +15,94 @@ interface User {
     first_name?: string;
     last_name?: string;
     role: { name: string };
+    is_active: boolean;
     created_at: string;
 }
 
 const UsersListPage = () => {
-    const [users, setUsers] = useState<User[]>([]);
+    const [usersCount, setUsersCount] = useState<number>(0);
+
+    const [activeUsers, setActiveUsers] = useState<User[]>([]);
+    const [archivedUsers, setArchivedUsers] = useState<User[]>([]);
+
     const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         api.get("/v1/Hub/Users/List").then(async (res) => {
             const data = await res.json();
-            setUsers(data.list);
+
+            setUsersCount(data.list.length);
+            setActiveUsers(data.list.filter((u: User) => u.is_active));
+            setArchivedUsers(data.list.filter((u: User) => !u.is_active));
+
             setLoading(false);
         });
     }, []);
 
-    return (
-        <DefaultPage
-            title="Користувачі"
-            action={
-                <Button adaptive={true} onClick={() => navigate("/users/add-user")}
-                >
-                    <Plus strokeWidth={2.25} />Додати
-                </Button>
-            }
-            isLoading={loading}
-        >
-            {users.length === 0 ? (
+    const usersColumns = [
+        {
+            key: "first_name",
+            label: "Користувач",
+            render: (v: string, row: User) => v && (
+                <UserLabel
+                    name={`${row.first_name} ${row.last_name || ""}`.trim()}
+                    avatar_url={row.avatar_url}
+                />
+            ),
+        },
+        { key: "role", label: "Роль", render: (v: UserRoleType) => v?.name || "—" },
+        { key: "email", label: "Email" },
+    ]
+
+    if (loading) {
+        return <DefaultPage title="Користувачі" isLoading={true} />;
+    }
+
+    if (usersCount === 0) {
+        return (
+            <DefaultPage
+                title="Користувачі"
+                action={
+                    <Button adaptive={true} onClick={() => navigate("/users/add-user")}
+                    >
+                        <Plus strokeWidth={2.25} />Додати
+                    </Button>
+                }
+            >
                 <p>Поки немає жодного користувача.</p>
-            ) : (
+            </DefaultPage>
+        );
+    }
+
+    return (
+        <>
+            <DefaultPage
+                title="Користувачі"
+                action={
+                    <Button adaptive={true} onClick={() => navigate("/users/add-user")}
+                    >
+                        <Plus strokeWidth={2.25} />Додати
+                    </Button>
+                }
+            >
                 <Table
-                    columns={[
-                        {
-                            key: "first_name",
-                            label: "Користувач",
-                            render: (v, row) => v && (
-                                <UserLabel
-                                    name={`${row.first_name} ${row.last_name || ""}`.trim()}
-                                    avatar_url={row.avatar_url}
-                                />
-                            ),
-                        },
-                        { key: "role", label: "Роль", render: (v) => v?.name || "—" },
-                        { key: "email", label: "Email" },
-                    ]}
-                    data={users}
+                    columns={usersColumns}
+                    data={activeUsers}
                     onRowClick={(row) => navigate(`/users/u/${row.id}`)}
                 />
-            )}
-        </DefaultPage>
+            </DefaultPage>
+
+            <DefaultPage
+                title="Архівні користувачі"
+            >
+                <Table
+                    columns={usersColumns}
+                    data={archivedUsers}
+                    onRowClick={(row) => navigate(`/users/u/${row.id}`)}
+                />
+            </DefaultPage>
+        </>
     );
 };
 
